72796bf9151b9de7cc57795c3799b0d208bec2e1
stuson <sam.tuson@hotmail.co.uk>
2019-04-19 11:18:16 +0100

    Create basic package structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="twitter-music",
+    version="0.0.1",
+    description="Generate music from a Twitter feed.",
+    author="Samuel Tuson",
+    packages=find_packages()
+)
